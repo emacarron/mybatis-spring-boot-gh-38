a0726c4f2e613c7598935c496b61951deead182b
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-03-29 02:46:51 +0900

    Add test case to open home

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -2,17 +2,29 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@WebIntegrationTest(randomPort = true)
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Value("${local.server.port}")
+    int port;
+
+    @Test
+    public void home() {
+        RestTemplate restTemplate = new RestTemplate();
+        String content = restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+        System.out.println(content);
+        assertThat(content, is("Hello!!"));
+    }
 
 }
