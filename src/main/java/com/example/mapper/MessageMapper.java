96a847a6a42f9d01329f15dc7d57b668d39b6bc3
Eduardo Macarron <eduardo.macarron@gmail.com>
2016-04-16 08:18:25 +0200

    Updated to mybatis 3.4.0

--- src/main/java/com/example/mapper/MessageMapper.java
+++ src/main/java/com/example/mapper/MessageMapper.java
@@ -1,7 +1,10 @@
 package com.example.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.example.model.Message;
 
+@Mapper
 public interface MessageMapper {
 
     Message findOne(String code);
